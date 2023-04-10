5eeb1c6714e3561e5d2e38824d147049f2cd7687
Spite <spite-vs@yandex.ru>
2023-04-10 15:43:44 +0300

    Задачи 64, 66, 68

---
+++ Task_66.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int m, n;
+Console.WriteLine("Введите число M:");
+int.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите число N:");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.Write($"M = {m}; N = {n} -> ");
+Console.Write($"{OutSum(m, n)}");
+
+
+int OutSum(int m, int n)
+{
+
+    int sum = m;
+    if (m >= n)
+        return sum;
+    return sum += OutSum(m + 1, n);
+}
---
+++ Task_68.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int m, n;
+Console.WriteLine("Введите число M:");
+int.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите число N:");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.Write($"M = {m}; N = {n} -> ");
+Console.Write($"A(m,n) = {Akker(m, n)}");
+
+
+int Akker(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Akker(m - 1, 1);
+    else
+        return Akker(m - 1, Akker(m, n - 1));
+}
